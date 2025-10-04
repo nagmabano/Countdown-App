049fd7b51b0111d575af9ce2ab15feb94637ae3f
nagma <bano.nagma.1@gmail.com>
2025-10-04 20:31:27 +0530

    solving the timer instance issue

--- src/components/TimerChallenge.jsx
+++ src/components/TimerChallenge.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 
+let timer; // Timer ID for managing the timeout, to solve our issue we can move the timer variable outside the component function.
+
 export default function TimerChallenge({ title, targetTime }) {
   const [timerStarted, setTimerStarted] = useState(false);
   const [timerExpired, setTimerExpired] = useState(false);
 
-  let timer; // Timer ID for managing the timeout, 
+//   let timer; // Timer ID for managing the timeout, 
   // If we keep it here a new variable will be created on each render and the timer for setting the timer will not be 
   // similar to the one for clearing the timer.
 
