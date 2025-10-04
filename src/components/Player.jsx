9de89984e124e5978075eb588a0c6ee1e2c2861a
nagma <bano.nagma.1@gmail.com>
2025-10-04 19:39:20 +0530

    Connecting and accessing html elements using ref

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,23 +1,18 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 export default function Player() {
   const [enteredPlayerName, setEnteredPlayerName] = useState("");
-  const [submitted, setSubmitted] = useState(false);
-
-  function handleChange(event) {
-    setSubmitted(false);
-    setEnteredPlayerName(event.target.value);
-  }
+  const playerName = useRef();
 
   function handleSubmit() {
-    setSubmitted(true);
+    setEnteredPlayerName(playerName.current.value);
   }
 
   return (
     <section id="player">
-      <h2>Welcome { submitted ? enteredPlayerName : 'unknown entity'} </h2>
+      <h2>Welcome { enteredPlayerName ? enteredPlayerName : 'unknown entity'} </h2>
       <p>
-        <input type="text" onChange={handleChange} value={enteredPlayerName}/>
+        <input ref={playerName} type="text"/>
         <button onClick={handleSubmit}>Set Name</button>
       </p>
     </section>
